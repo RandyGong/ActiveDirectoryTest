b5b66157f0bd41215f116ad8d567ddc9cb416d24
RandyGong <attackwc@163.com>
2013-10-18 17:59:36 +0800

    2

---
+++ ActiveDirectoryTest/ADS_USER_FLAG_ENUM.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActiveDirectoryTest
+{
+    ///  
+    ///用户属性定义标志  
+    ///  
+    public enum ADS_USER_FLAG_ENUM
+    {
+        ///  
+        ///登录脚本标志。如果通过 ADSI LDAP 进行读或写操作时，该标志失效。如果通过 ADSI WINNT，该标志为只读。  
+        ///  
+        ADS_UF_SCRIPT = 0X0001,
+        ///  
+        ///用户帐号禁用标志  
+        ///  
+        ADS_UF_ACCOUNTDISABLE = 0X0002,
+        ///  
+        ///主文件夹标志  
+        ///  
+        ADS_UF_HOMEDIR_REQUIRED = 0X0008,
+        ///  
+        ///过期标志  
+        ///  
+        ADS_UF_LOCKOUT = 0X0010,
+        ///  
+        ///用户密码不是必须的  
+        ///  
+        ADS_UF_PASSWD_NOTREQD = 0X0020,
+        ///  
+        ///密码不能更改标志  
+        ///  
+        ADS_UF_PASSWD_CANT_CHANGE = 0X0040,
+        ///  
+        ///使用可逆的加密保存密码  
+        ///  
+        ADS_UF_ENCRYPTED_TEXT_PASSWORD_ALLOWED = 0X0080,
+        ///  
+        ///本地帐号标志  
+        ///  
+        ADS_UF_TEMP_DUPLICATE_ACCOUNT = 0X0100,
+        ///  
+        ///普通用户的默认帐号类型  
+        ///  
+        ADS_UF_NORMAL_ACCOUNT = 0X0200,
+        ///  
+        ///跨域的信任帐号标志  
+        ///  
+        ADS_UF_INTERDOMAIN_TRUST_ACCOUNT = 0X0800,
+        ///  
+        ///工作站信任帐号标志  
+        ///  
+        ADS_UF_WORKSTATION_TRUST_ACCOUNT = 0x1000,
+        ///  
+        ///服务器信任帐号标志  
+        ///  
+        ADS_UF_SERVER_TRUST_ACCOUNT = 0X2000,
+        ///  
+        ///密码永不过期标志  
+        ///  
+        ADS_UF_DONT_EXPIRE_PASSWD = 0X10000,
+        ///  
+        /// MNS 帐号标志  
+        ///  
+        ADS_UF_MNS_LOGON_ACCOUNT = 0X20000,
+        ///  
+        ///交互式登录必须使用智能卡  
+        ///  
+        ADS_UF_SMARTCARD_REQUIRED = 0X40000,
+        ///  
+        ///当设置该标志时，服务帐号（用户或计算机帐号）将通过 Kerberos 委托信任  
+        ///  
+        ADS_UF_TRUSTED_FOR_DELEGATION = 0X80000,
+        ///  
+        ///当设置该标志时，即使服务帐号是通过 Kerberos 委托信任的，敏感帐号不能被委托  
+        ///  
+        ADS_UF_NOT_DELEGATED = 0X100000,
+        ///  
+        ///此帐号需要 DES 加密类型  
+        ///  
+        ADS_UF_USE_DES_KEY_ONLY = 0X200000,
+        ///  
+        ///不要进行 Kerberos 预身份验证  
+        ///  
+        ADS_UF_DONT_REQUIRE_PREAUTH = 0X4000000,
+        ///  
+        ///用户密码过期标志  
+        ///  
+        ADS_UF_PASSWORD_EXPIRED = 0X800000,
+        ///  
+        ///用户帐号可委托标志  
+        ///  
+        ADS_UF_TRUSTED_TO_AUTHENTICATE_FOR_DELEGATION = 0X1000000
+    }
+}
--- ActiveDirectoryTest/Program.cs
+++ ActiveDirectoryTest/Program.cs
@@ -14,11 +14,12 @@ namespace ActiveDirectoryTest
             var dirEntry = GetDirectoryObject();
             Console.WriteLine("Hello from outside. dirEntry.Name = " + dirEntry.Name);
 
-            Console.WriteLine("Is g exist : " + ExistWinUser("g"));
+            Console.WriteLine("Is userFromCode exist : " + ExistWinUser("userFromCode"));
 
-            //CreateLocalUser("userFromCode", "123456", "");
-
-            adduser("userFromCodeLater");
+            //CreateLocalUser("userFromCode", "abcd1234*", "");
+            //SetEnable("userFromCode");
+            EnableUser("userFromCode");
+            //adduser("userFromCodeLater");
             Console.Read();
         }
 
@@ -31,7 +32,7 @@ namespace ActiveDirectoryTest
             try
             {
                 //entry = new DirectoryEntry("LDAP://localhost", "GongJiChang", "abcd1234*", AuthenticationTypes.Secure);
-                entry = new DirectoryEntry("WinNT://localhost", "GongJiChang", "abcd1234*", AuthenticationTypes.Secure);
+                entry = new DirectoryEntry("WinNT://localhost", "Administrator", "abcd1234*", AuthenticationTypes.Secure);
             }
             catch (Exception ex)
             {
@@ -50,6 +51,7 @@ namespace ActiveDirectoryTest
             //DirectoryEntry localMachine = new DirectoryEntry("WinNT://" + Environment.MachineName + ",computer");
             var localMachine = GetDirectoryObject();
             var newUser = localMachine.Children.Add(username, "user");
+            //newUser.Properties["userAccountControl"].Value = 544;
             newUser.Invoke("SetPassword", new object[] { password });
             newUser.Invoke("Put", new object[] { "Description", description });
             newUser.CommitChanges();
@@ -65,12 +67,69 @@ namespace ActiveDirectoryTest
             DirectoryEntries users = decu.Children;
             DirectoryEntry user = users.Add("CN=" + addaccount, "user");
 
-            user.Properties["userPrincipalName"].Add(addaccount);
-            user.Properties["samAccountName"].Add(addaccount);    //添加用户的帐号名称  
-            user.Properties["pwdLastSet"].Value = 0;           //设置上一次登陆密码为空,用户在新登陆后需要重新设置密码  
-            user.Properties["userAccountControl"].Value = 544;   //有效用户   ,应该设为512,却出错  
+            //user.Properties["userPrincipalName"].Add(addaccount);
+            //user.Properties["sAMAccountName"].Add(addaccount);    //添加用户的帐号名称  
+            //user.Properties["sAMAccountName"].Value = addaccount; 
+            //user.Properties["pwdLastSet"].Value = 0;           //设置上一次登陆密码为空,用户在新登陆后需要重新设置密码  
+            //user.Properties["userAccountControl"].Value = 544;   //有效用户 启用用户  ,应该设为512,却出错  
             user.CommitChanges();                                                 //确认改变,写入AD  
+            user.Close();
+            decu.Close();
+        }
 
+        public static void SetEnable(string user)
+        {
+            DirectoryEntry ude = new DirectoryEntry("WinNT://localhost", user, "abcd1234*", AuthenticationTypes.Secure);
+            ude.Properties["userAccountControl"].Value = 544;
+            ude.CommitChanges();
+            ude.Close();
+
+        }
+
+        ///  
+        ///根据用户公共名称取得用户的 对象  
+        ///  
+        ///  用户公共名称   
+        ///如果找到该用户，则返回用户的 对象；否则返回 null  
+        public static DirectoryEntry GetDirectoryEntry(string commonName)
+        {
+            DirectoryEntry de = GetDirectoryObject();
+            DirectorySearcher deSearch = new DirectorySearcher(de);
+            deSearch.Filter = "(&(&(objectCategory=person)(objectClass=user))(cn=" + commonName + "))";
+            deSearch.SearchScope = SearchScope.Subtree;
+            try
+            {
+                SearchResult result = deSearch.FindOne();
+                de = new DirectoryEntry(result.Path);
+                return de;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        private static IdentityImpersonation impersonate = new IdentityImpersonation("Administrator", "abcd1234*", "localhost");
+
+        ///  
+        ///启用指定公共名称的用户  
+        ///  
+        ///  用户公共名称   
+        public static void EnableUser(string commonName)
+        {
+            EnableUser(GetDirectoryEntry(commonName));
+        }
+        ///  
+        ///启用指定 的用户  
+        ///  
+        ///  
+        public static void EnableUser(DirectoryEntry de)
+        {
+            impersonate.BeginImpersonate();
+            de.Properties["userAccountControl"][0] = ADS_USER_FLAG_ENUM.ADS_UF_NORMAL_ACCOUNT | ADS_USER_FLAG_ENUM.ADS_UF_DONT_EXPIRE_PASSWD;
+            de.CommitChanges();
+            impersonate.StopImpersonate();
+            de.Close();
         }
 
         /// <summary>
